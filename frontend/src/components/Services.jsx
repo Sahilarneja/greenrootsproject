7f706fca14cf923ef15157f25bb933bbae482a7e
sahil <sahilarneja2003@gmail.com>
2024-07-08 14:20:56 +0530

    updated

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSolarPanel, faUnlockAlt, faMoneyBill } from '@fortawesome/free-solid-svg-icons';
 import '../styles/services.css';
-
 
 const servicesData = [
     {
-        
+        icon: faSolarPanel,
         heading: 'RoofTop Solar Installation',
         description: 'Rooftop solar installation harnesses sunlight to generate clean electricity, reducing energy costs and environmental impact',
     },
     {
-        
+        icon: faUnlockAlt,
         heading: 'Access Control',
         description: 'Open access solar enables consumers to choose renewable energy sources, providing greater flexibility and sustainability in electricity consumption.',
     },
     {
-       
-        heading: 'COPEX /OPEX',
+        icon: faMoneyBill,
+        heading: 'COPEX / OPEX',
         description: 'COPEX (Capital Expenditure) refers to the upfront costs of acquiring and installing assets, while OPEX (Operational Expenditure) covers the ongoing costs of operating and maintaining them',
     },
 ];
@@ -25,7 +26,11 @@ function Services() {
         <div className="services-container">
             {servicesData.map((service, index) => (
                 <div className="service-box" key={index}>
-                    {service.icon && <div className="service-icon">{service.icon}</div>}
+                    {service.icon && (
+                        <div className="service-icon">
+                            <FontAwesomeIcon icon={service.icon} />
+                        </div>
+                    )}
                     <div className="service-content">
                         <div className="service-heading">{service.heading}</div>
                         <div className="service-description">{service.description}</div>
